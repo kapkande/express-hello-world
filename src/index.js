e74c34307009592534940c81c2fdde823949cea6
Po51 <Po51@dc.liftservis.by>
2023-03-16 16:19:43 +0300

    fix: port

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// import './assets/styles/index.css';
+// import satMainPage from './assets/components/mainPage/satMainPage'
+// import satDonatePage from './assets/components/donatePage/setDonatePage';
+// satDonatePage()
+
+
+
+// function satMainPage () {
+//     const main = document.querySelector('.main')
+//     main.innerHTML='<a class="header__nav__item link" href="">Zoos</a>'
+// } 
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+
+const PORT = process.env.PORT || 3000
+app.set('views', path.resolve(__dirname, 'ejs'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.resolve(__dirname, 'assets')))
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'about' });
+})
+app.get('/donate', (req, res) => {
+    res.render('donate', { title: 'donate' });
+})
+app.listen(PORT, () => {
+    console.log('server has been started ' + PORT);
+})
